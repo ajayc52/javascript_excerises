82f221653415a7e9234572a40151efc5575c45bc
ajayc52 <ajaykumarc52@gmail.com>
2023-03-20 21:18:37 -0500

    commiting check age function

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
-//writing my first javascript
+// // //writing my first javascript
+
+// // // Write a function called "checkAge". Given a person's name and age, "checkAge" returns one of two messages: "Go home, {insert_name_here}!",
+// // if they are younger than 21. "Welcome, {insert_name_here}!",
+// // if they are 21 or older. Naturally, replace "{insert_name_here}" with the given name. :)
+
+// Output
+
+// var output = checkAge('Adrian', 22);
+// console.log(output); // --> 'Welcome, Adrian!'
+
+// Create a dunction signature
+
+function checkAge(name, age) {
+    // Check whether the age is 21 
+    if (age >= 21) {
+        return "welcome, " + name + "!"
+    }
+    return "go back home, " + name + "!";
+    // if age is 21 welcome message should be displayed
+    // if not go back to home
+
+};
+console.log(checkAge("Ajay", 27));
+console.log(checkAge("Louis", 20));
