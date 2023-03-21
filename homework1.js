42b8e083fdf30f0b275737beda8b6a9db97a83b8
ajayc52 <ajaykumarc52@gmail.com>
2023-03-21 17:21:27 -0500

    Homework

---
+++ homework1.js
@@ -0,0 +1,54 @@
+//1
+function isGreaterThan10(number) {
+    // condition
+    return number > 10;
+}
+var output = isGreaterThan10(11);
+console.log(output);
+
+// 2
+
+function islessthan30(number) {
+    return number < 30;
+}
+var output = islessthan30(9)
+console.log(output);
+
+//3
+function equalsTen(number) {
+    return number == 10;
+
+}
+var output = equalsTen(9);
+console.log(output);
+
+
+
+//4
+function isLessThan(number1, number2) {
+    return number2 < number1;
+
+}
+var output = isLessThan(9, 4)
+console.log(output);
+
+
+
+
+// 5
+function isGreaterThan(number1, number2) {
+    return number2 > number1;
+
+
+}
+var output = isGreaterThan(11, 10);
+console.log(output);
+
+
+//5
+function isEqualTo(number1, number2) {
+    return number2 == number1;
+
+}
+var output = isEqualTo(11, 10);
+console.log(output)
